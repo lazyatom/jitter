ae1b530b1249e3b8c984d513c89cf25e5f04fd18
James Adam <james@lazyatom.com>
2009-01-27 13:19:35 +0000

    Wild refactoring and bugfixing.

--- jitter.rb
+++ jitter.rb
@@ -4,7 +4,6 @@ require 'rubygems'
 require 'xmpp4r-simple'
 require 'twitter'
 require 'yaml'
-require 'tempfile'
 require 'cgi'
 
 config = YAML.load_file(File.expand_path("~/.jitter.yaml"))
@@ -12,42 +11,52 @@ config = YAML.load_file(File.expand_path("~/.jitter.yaml"))
 im = Jabber::Simple.new(config[:jabber][:account], config[:jabber][:password])
 twitter = Twitter::Base.new(config[:twitter][:account], config[:twitter][:password])
 
-tmp = Tempfile.new('most_recent_status_timestamp')
-last_seen_path = tmp.path
-tmp.close!
+def post_update_to_twitter(im, twitter, config)
+  im.received_messages do |message|
+    if config[:accept_from].include?(message.from.strip.to_s)
+      twitter.post(message.body, :source => "Jabber")
+    end
+  end
+end
+
+def new_messages(twitter)
+  last_seen_path = File.expand_path("~/.jitter.last_seen") 
+  last_seen = Time.parse(File.read(last_seen_path)) rescue Time.new - (60*60*24) # yesterday
+  messages = twitter.timeline.select { |status| Time.parse(status.created_at) > last_seen }.reverse
+  if messages.any?
+    File.open(last_seen_path, 'w') do |f| 
+      f.write messages.last.created_at
+    end
+  end
+  messages
+end
+
+def post_tweets_to_im(im, twitter, config)
+  messages = new_messages(twitter)
+  if config[:show_messages_from] && config[:restrict_shown_messages]
+    messages.delete_if { |s| !config[:show_messages_from].include?(s.user.screen_name) }
+  end
+  messages.each do |status|
+    config[:accept_from].each do |user|
+      message = CGI.unescapeHTML(status.text)
+      tweeter = status.user.name
+      im.deliver(user, "[#{tweeter}] #{message}") rescue nil
+    end
+  end
+end
 
 updates = Thread.new do
   loop do
-    im.received_messages do |message|
-      if config[:accept_from].include?(message.from.strip.to_s)
-        puts "Posting '#{message.body}' to twitter."
-        twitter.post(message.body, :source => "Jabber")
-      else
-        puts "Rejecting message from #{message.from} (#{message.body})"
-      end
-    end
+    post_update_to_twitter(im, twitter, config)
     sleep(1)
   end
 end
 
 timeline = Thread.new do
   loop do
-    last_seen = Time.parse(File.read(last_seen_path)) rescue Time.new - (60*60*24) # yesterday
-    messages = twitter.timeline.select do |status| 
-      config[:show_messages_from].include?(status.user.screen_name) && 
-        Time.parse(status.created_at) > last_seen
-    end
-    puts "Found #{messages.length} new messages"
-    File.open(last_seen_path, 'w') { |f| f.puts messages.first.created_at.to_yaml } if messages.any?
-    messages.each do |status|
-      config[:accept_from].each do |user|
-        puts "sending to #{user}"
-        im.deliver(user, "[#{status.user.name}] #{CGI.unscapeHTML(status.text)}") rescue nil
-      end
-    end
+    post_tweets_to_im(im, twitter, config)
     sleep(30)
   end
 end
 
-updates.join
-timeline.join
+[updates, timeline].map { |t| t.join }
