5aad9fe13652e7994b51c54b241bf9adc02642c5
James Adam <james@lazyatom.com>
2009-01-29 22:34:16 +0000

    Refactored with some tests.

--- jitter.rb
+++ jitter.rb
@@ -6,57 +6,73 @@ require 'twitter'
 require 'yaml'
 require 'cgi'
 
-config = YAML.load_file(File.expand_path("~/.jitter.yaml"))
+class Jitter
+  attr_reader :current_status, :jabber, :twitter, :config
+  def initialize(config=YAML.load_file(File.expand_path("~/.jitter.yaml")))
+    @config = config
+    @jabber = Jabber::Simple.new(config[:jabber][:account], config[:jabber][:password])
+    @twitter = Twitter::Base.new(config[:twitter][:account], config[:twitter][:password])
+    @current_status = nil
+  end
 
-im = Jabber::Simple.new(config[:jabber][:account], config[:jabber][:password])
-twitter = Twitter::Base.new(config[:twitter][:account], config[:twitter][:password])
-
-def post_update_to_twitter(im, twitter, config)
-  im.received_messages do |message|
-    if config[:accept_from].include?(message.from.strip.to_s)
-      twitter.post(message.body, :source => "Jabber")
+  def post_update_to_twitter
+    jabber.presence_updates do |update|
+      account, presence, message = update
+      if config[:accept_from].include?(account) && message && message != current_status
+        twitter.post(message, :source => "Jitter")
+        @current_status = message
+      end
     end
   end
-end
 
-def new_messages(twitter)
-  last_seen_path = File.expand_path("~/.jitter.last_seen") 
-  last_seen = Time.parse(File.read(last_seen_path)) rescue Time.new - (60*60*24) # yesterday
-  messages = twitter.timeline.select { |status| Time.parse(status.created_at) > last_seen }.reverse
-  if messages.any?
-    File.open(last_seen_path, 'w') do |f| 
-      f.write messages.last.created_at
+  def post_tweets_to_im(im, twitter, config)
+    new_messages(twitter).each do |status|
+      config[:accept_from].each do |user|
+      end
     end
   end
-  messages
-end
 
-def post_tweets_to_im(im, twitter, config)
-  messages = new_messages(twitter)
-  if config[:show_messages_from] && config[:restrict_shown_messages]
-    messages.delete_if { |s| !config[:show_messages_from].include?(s.user.screen_name) }
+  private
+  
+  def send_tweet_to_jabber(tweet, jabber_account)
+    begin
+      message = CGI.unescapeHTML(tweet.text)
+      jabber.deliver(jabber_account, "[#{tweet.user.name}] #{message}")
+    rescue
+    end    
   end
-  messages.each do |status|
-    config[:accept_from].each do |user|
-      message = CGI.unescapeHTML(status.text)
-      tweeter = status.user.name
-      im.deliver(user, "[#{tweeter}] #{message}") rescue nil
+
+  def new_messages(twitter)
+    last_seen = Time.parse(File.read(last_seen_path)) rescue Time.new - (60*60*24) # yesterday
+    messages = twitter.timeline.select { |status| Time.parse(status.created_at) > last_seen }.reverse
+    if messages.any?
+      File.open(last_seen_path, 'w') do |f| 
+        f.write messages.last.created_at
+      end
     end
+    messages
   end
+  
+  def last_seen_path
+    @last_seen_path ||= File.expand_path("~/.jitter.last_seen")
+  end
+  
 end
 
-updates = Thread.new do
-  loop do
-    post_update_to_twitter(im, twitter, config)
-    sleep(1)
-  end
+if __FILE__ == $0
+  jitter = Jitter.new
+  [
+    Thread.new do
+      loop do
+        jitter.post_update_to_twitter
+        sleep(1)
+      end
+    end# , 
+    #   Thread.new do
+    #     loop do
+    #       jitter.post_tweets_to_im
+    #       sleep(30)
+    #     end
+    #   end
+  ].map { |t| t.join }
 end
-
-timeline = Thread.new do
-  loop do
-    post_tweets_to_im(im, twitter, config)
-    sleep(30)
-  end
-end
-
-[updates, timeline].map { |t| t.join }
---
+++ jitter_test.rb
@@ -0,0 +1,84 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'mocha'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'jitter'
+
+class JitterTest < Test::Unit::TestCase
+  
+  context "when listening for updates" do
+    setup do
+      @jabber = stub_jabber
+      @twitter = stub_twitter
+      @jitter = Jitter.new(config(:accept_from => ["james@lazyatom.com"]))
+    end
+    
+    should "ignore updates from accounts it doesn't know" do
+      @jabber.stubs(:presence_updates).yields(["notjames@lazyatom.com", :online, nil])
+      @twitter.expects(:post).never
+      @jitter.post_update_to_twitter
+    end
+    
+    should "update twitter when status message is changed" do
+      @jabber.stubs(:presence_updates).yields(["james@lazyatom.com", :online, "is happy as larry"])
+      @twitter.expects(:post).with("is happy as larry", anything)
+      @jitter.post_update_to_twitter
+    end
+    
+    should "not update twitter if the message is not present" do
+      @jabber.stubs(:presence_updates).yields(["james@lazyatom.com", :online, nil])
+      @twitter.expects(:post).never
+      @jitter.post_update_to_twitter
+    end
+    
+    should "not update twitter if the message is not changed" do
+      @jabber.stubs(:presence_updates).yields(["james@lazyatom.com", :online, "is happy"])
+      @jitter.post_update_to_twitter
+      
+      @jabber.stubs(:presence_updates).yields(["james@lazyatom.com", :away, "is happy"])
+      @twitter.expects(:post).never
+      @jitter.post_update_to_twitter
+    end
+  end
+  
+  context "when listening to twitter" do
+    setup do
+      @jabber = stub_jabber
+      @jitter = stub_twitter
+      @jitter = Jitter.new
+    end
+    
+    should "send updates to jabber" do
+      
+    end
+  end
+  
+  private
+  
+  def config(overrides={})
+    {
+      :accept_from => ["test@example.com"],
+      :jabber => {
+        :account => "test@example.com",
+        :password => "password"
+      },
+      :twitter => {
+        :account => "test@example.com",
+        :password => "password"
+      }
+    }.merge(overrides)
+  end
+  
+  def stub_jabber
+    Jabber::Simple.stubs(:new).returns(jabber = stub_everything('jabber stub'))
+    return jabber
+  end
+  
+  def stub_twitter
+    Twitter::Base.stubs(:new).returns(twitter = stub_everything('twitter stub'))
+    return twitter
+  end
+  
+end
